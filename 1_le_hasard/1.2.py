3b9a6280375d1f1796d9a01ae902aa1184d4438a
Alexandre <moreaualexandre2002@gmail.com>
2024-03-28 14:48:00 +0100

    commit

--- 1_le_hasard/1.2.py
+++
@@ -1,5 +0,0 @@
-from random import choice
-
-liste = [1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 6, 6]
-for x in range(100):
-    print(choice(liste), end=" ")
--- README.md
+++
@@ -1,1 +0,0 @@
-# Apprendre en ligne - Pyhton
