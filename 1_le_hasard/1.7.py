12efd39f60430cdaacc28a9dd377409161fe1a21
Alexandre <moreaualexandre2002@gmail.com>
2024-03-28 14:47:21 +0100

    commit

--- 1_le_hasard/1.7.py
+++
@@ -1,7 +0,0 @@
-from random import *
-
-for x in range(100):
-    if random() < 0.5783:
-        print("f", end=" ")
-    else:
-        print("f", end=" ")
--- 2_devine_mon_nombre/2.1.py
+++
@@ -1,9 +0,0 @@
-word = "none"
-counter = 0
-
-while word != "":
-    counter += 1
-    word = input("Ecrivez un mot (ou juste ENTRER pour quitter) : ")
-    print(counter, word)
-
-print("Au revoir !")
