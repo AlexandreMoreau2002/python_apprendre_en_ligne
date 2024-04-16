2a6a109035a17ad45f70347aa4e7c30d84d405cf
Alexandre <moreaualexandre2002@gmail.com>
2024-04-16 23:56:27 +0200

    feat(exo 4.1): exo 4.1

--- 4_pierre_papier_ciseaux(graphique)/4.1.py
+++ 4_pierre_papier_ciseaux(graphique)/4.1.py
@@ -1,1 +1,32 @@
-from tkinter import *
+from tkinter import Tk, Label, Button
+
+counter = 0
+
+
+def incrementer():
+    global counter
+    counter += 1
+    ligne1.configure(text=str(counter))
+
+
+def restart():
+    global counter
+    counter = 0
+    ligne1.configure(text=str(counter))
+
+
+fenetre = Tk()
+fenetre.title("Bouton incrementeur")
+fenetre.geometry("400x400")
+
+ligne1 = Label(fenetre, text=counter)
+ligne1.grid(row=0, column=1,)
+
+bouton1 = Button(fenetre, text="Incrementer", command=incrementer, width=6, height=2)
+bouton1.grid(row=1, column=0)
+bouton2 = Button(fenetre, text="Recommencer", command=restart, width=6, height=2)
+bouton2.grid(row=1, column=1)
+bouton3 = Button(fenetre, text="Quitter", command=fenetre.destroy, width=6, height=2)
+bouton3.grid(row=1, column=2)
+
+fenetre.mainloop()
