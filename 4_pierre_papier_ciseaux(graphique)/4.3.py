1b8aebd3963e2f336dbc42492091175eb5c4cce6
Alexandre <moreaualexandre2002@gmail.com>
2024-04-17 01:06:27 +0200

    feat(add 4.3): 4.3

---
+++ 4_pierre_papier_ciseaux(graphique)/4.3.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+fenetre = Tk()
+fenetre.title("Pierre, Papier, Ciseaux")
+
+pierre = PhotoImage(
+    file="/Users/alex/Desktop/python_apprendre_en_ligne/4_pierre_papier_ciseaux(graphique)/pierre.gif"
+)
+
+for i in range(10):
+    lab = Label(fenetre, image=pierre)
+    lab.grid(row=0, column=i)
+
+# or with while
+# i = 0
+# while i < 10:
+#     lab = Label(fenetre, image=pierre)
+#     lab.grid(row=0, column=i)
+#     i += 1
+
+fenetre.mainloop()
