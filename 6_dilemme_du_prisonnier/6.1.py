7aa3a1316c47101d1df717099947799a6417ee3d
Alexandre <moreaualexandre2002@gmail.com>
2024-05-11 00:08:31 +0200

    feat: 6.1

---
+++ 6_dilemme_du_prisonnier/6.1.py
@@ -0,0 +1,18 @@
+from pprint import pprint
+
+dico = {
+    "Janvier": 31,
+    "Février": 28,
+    "Mars": 31,
+    "Avril": 30,
+    "Mai": 31,
+    "Juin": 30,
+    "Juillet": 31,
+    "Aoùt": 31,
+    "Septembre": 30,
+    "Octobre": 31,
+    "Novembre": 30,
+    "Décembre": 31,
+}
+
+pprint(dico)
