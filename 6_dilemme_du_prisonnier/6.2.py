a7ef083967865b8768357561e145f9475498cda3
Alexandre <moreaualexandre2002@gmail.com>
2024-06-03 00:16:42 +0200

    chore(inverser): other function inverser methode

--- 6_dilemme_du_prisonnier/6.2.py
+++ 6_dilemme_du_prisonnier/6.2.py
@@ -21,3 +21,25 @@ def inverser(dico):
 
 print()
 pprint(inverser(dico))
+
+# Version un peu plus intéressante : 
+
+dico2 = {
+    "computer": "ordi",
+    "mouse": "souris",
+    "keyboard": "clavier",
+}
+
+
+def inverser():
+    global dico2
+    print(f"dico avant {dico2}")
+    dico_inv2 = {}
+    for cle, valeur in dico2.items():
+        dico_inv2[valeur] = cle
+    dico2 = dico_inv2
+    print(f"dico apres {dico2}")
+
+
+inverser()
+inverser()
